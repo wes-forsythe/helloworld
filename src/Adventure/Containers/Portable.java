ff90baac1d413d157f79edcb2f8a19f0c36c2fbb
Wes Forsythe <wesforsythe09@gmail.com>
2012-02-26 17:47:26 -0600

    Added container package

---
+++ src/Adventure/Containers/Portable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Containers;
+
+/**
+ *
+ * @author Wes
+ */
+public class Portable {
+    private boolean dropable = false;
+    private boolean InTransit = true;
+    
+    void getAttribute(){
+        
+    }
+    void setAttribute(){
+        
+    }
+};
